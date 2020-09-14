5f2373f1f76c074b04d9348a122137090e0c9e09
Praveen manchi <49606401+praveenmanchi@users.noreply.github.com>
2020-09-14 19:13:15 +0530

    min changes
    sasidhar

--- js/customJs/designDemos.js
+++ js/customJs/designDemos.js
@@ -1,25 +1,28 @@
 (function ($) {
-	'use strict';
+	// 'use strict';
 
 	jQuery(document).ready(function () {
 
         $('.design-images .image-container').on('click', (event) => {
-            const clickedElement = event.target;
+            const clickedElement = event.currentTarget;
             const clickedTagName = clickedElement.tagName;
-            let clickedImgSrc = '';
+            let clickedImgSrc_data = '';
             switch (clickedTagName) {
                 case 'SPAN':
-                    clickedImgSrc = clickedElement.parentElement.querySelector('img').src;
+                    clickedImgSrc_data = $(clickedElement).parent('img').data('slide');
                     break;
                 case 'DIV':
-                    clickedImgSrc = clickedElement.querySelector('img').src;
+                    clickedImgSrc_data = $(clickedElement).find('img').data('slide');
                     break;
                 default:
-                    clickedImgSrc = clickedElement.src;
+                    clickedImgSrc_data = $(clickedElement).data('slide');
             }
-            const targetImg = $('.design-container').find('img');
-            if (targetImg.attr('src') !== clickedImgSrc)
-                targetImg.attr('src', clickedImgSrc);
+            const targetImg = $('.design-container').find('.active');
+            var active_data = targetImg.data('slide')
+            if(active_data !== clickedImgSrc_data){
+                targetImg.removeClass('active')
+                $('.design-container').find('img[data-slide='+clickedImgSrc_data+']').addClass('active')
+            }
         });
 	});
 })(jQuery);
